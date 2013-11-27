ffc8188b36f45bf7e6825d43d57b904d32641952
Albert Gazizov <deeper4k@gmail.com>
2013-11-27 21:42:17 +0400

    Added ValidationRules.find_by_validator_name method

--- lib/attr_validator/validation_rules.rb
+++ lib/attr_validator/validation_rules.rb
@@ -7,4 +7,9 @@ module AttrValidator::ValidationRules
   require 'attr_validator/validation_rules/length_validation_rule'
   require 'attr_validator/validation_rules/numericality_validation_rule'
   require 'attr_validator/validation_rules/regexp_validation_rule'
+
+  def self.find_by_validator_name(validator_name)
+    validator = AttrValidator::Validators.find_by_name!(validator_name)
+    validator.validation_rule_class
+  end
 end
