b7dfc4539f6f76b9e84d8a31fa7a53332fd60f78
Albert Gazizov <deeper4k@gmail.com>
2014-06-06 13:02:56 +0400

    Added ru localization

--- lib/attr_validator/i18n.rb
+++ lib/attr_validator/i18n.rb
@@ -2,6 +2,6 @@ class AttrValidator::I18n
 
   # Translates message to specific language
   def self.t(message, options = {})
-    I18n.t(message, options)
+    I18n.t("attr_validator.#{message}", options)
   end
 end
